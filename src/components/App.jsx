693c706c9abd436af43c04c61199bb379305db0d
Houssein Windows <houssein.msallem@gmail.com>
2024-01-22 12:51:43 +0100

    Made form inputs write out interactively.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
-import '../styles/App.css';
-import { useState } from 'react';
+import "../styles/App.css";
+import { useState } from "react";
 function App() {
-  const [fullname, setFullname] = useState('');
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [github, setGithub] = useState("");
   function handleFullnameChange(e) {
     setFullname(e.target.value);
   }
-
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+  function handlePhoneChange(e) {
+    setPhone(e.target.value);
+  }
+  function handleGithubChange(e) {
+    setGithub(e.target.value);
+  }
   return (
     <>
       <h1 className="bigHeader">Build Your CV TODAY!</h1>
@@ -17,7 +28,7 @@ function App() {
             </label>
             <div>
               <label htmlFor="Enter your Fullname">
-                *Fullname:{' '}
+                *Fullname:{" "}
                 <input
                   type="text"
                   name="fullname"
@@ -29,18 +40,38 @@ function App() {
             </div>
             <div>
               <label htmlFor="Enter your Email">
-                *Email: <input type="mail" name="email" id="email" />
+                *Email:{" "}
+                <input
+                  type="mail"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
               </label>
             </div>
             <div>
               <label htmlFor="Enter your Phone Number">
-                *Telephone:{' '}
-                <input type="number" name="fullname" id="fullname" />
+                *Telephone:{" "}
+                <input
+                  type="number"
+                  name="phone"
+                  id="phone"
+                  value={phone}
+                  onChange={handlePhoneChange}
+                />
               </label>
             </div>
             <div>
               <label htmlFor="Enter your Github Profile Link">
-                *Github: <input type="text" name="fullname" id="fullname" />
+                *Github:{" "}
+                <input
+                  type="text"
+                  name="github"
+                  id="github"
+                  value={github}
+                  onChange={handleGithubChange}
+                />
               </label>
             </div>
             <button type="submit">Submit</button>
@@ -48,6 +79,8 @@ function App() {
         </fieldset>
         <div className="preview">
           <h3>{fullname}</h3>
+          <span>{email}</span>
+          <span>{phone}</span>
         </div>
       </div>
     </>
